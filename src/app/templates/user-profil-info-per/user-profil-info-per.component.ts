407bb5d684d68f6275091aa6647409ed1d4e4a88
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-05 23:14:09 +0100

    up

--- src/app/templates/user-profil-info-per/user-profil-info-per.component.ts
+++ src/app/templates/user-profil-info-per/user-profil-info-per.component.ts
@@ -1,18 +1,17 @@
 import { Component } from "@angular/core";
-
+import { AuthService } from "src/app/auth.service";
 @Component({
   selector: "app-user-profil-info-per",
   templateUrl: "./user-profil-info-per.component.html",
   styleUrls: ["./user-profil-info-per.component.scss"],
 })
 export class UserProfilInfoPerComponent {
-  list: any = [
-    {
-      img: "./../../../assets/Mask.png",
-      nom: "Saif",
-      prenom: "Abid",
-      email: "saif.abid@gmail.com",
-      password: " ****** ",
-    },
-  ];
+  user: any;
+  constructor(private auth: AuthService) {}
+  ngOnInit() {
+    this.auth.verify().subscribe((data) => {
+      console.log(data);
+      this.user = data;
+    });
+  }
 }
