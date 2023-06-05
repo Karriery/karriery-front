7c164b5d8e83b4a1b7e77376a2d0498d1a8c401b
halim-boussada <halimboussada10@gmail.com>
2023-06-05 21:06:29 +0200

    osle

--- src/app/shared/create-user-card-user1/create-user-card-user1.component.ts
+++ src/app/shared/create-user-card-user1/create-user-card-user1.component.ts
@@ -1,18 +1,26 @@
 import { Component } from "@angular/core";
-
+import { AuthService } from "src/app/auth.service";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-create-user-card-user1",
   templateUrl: "./create-user-card-user1.component.html",
   styleUrls: ["./create-user-card-user1.component.scss"],
 })
 export class CreateUserCardUser1Component {
-  list: any = [
-    {
-      img: "./../../../assets/Mask.png",
-      nom: "Saif",
-      prenom: "Abid",
-      email: "saif.abid@gmail.com",
-      password: " ******** ",
-    },
-  ];
+
+  user : any 
+  constructor(private auth : AuthService , private router : Router){}
+  ngOnInit(){
+    this.auth.verify().subscribe(
+      data => {
+        console.log(data)
+        this.user = data
+      },
+      err => {
+        localStorage.clear()
+        this.router.navigateByUrl('/')
+      }
+    );;
+  }
 }
+
--- src/app/templates/create-user-caree-information/create-user-caree-information.component.ts
+++ src/app/templates/create-user-caree-information/create-user-caree-information.component.ts
@@ -1,8 +1,29 @@
 import { Component } from "@angular/core";
-
+import { AuthService } from "src/app/auth.service";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-create-user-caree-information",
   templateUrl: "./create-user-caree-information.component.html",
   styleUrls: ["./create-user-caree-information.component.scss"],
 })
-export class CreateUserCareeInformationComponent {}
+export class CreateUserCareeInformationComponent {
+  user : any 
+  constructor(private auth : AuthService , private router : Router){}
+  ngOnInit(){
+    this.auth.verify().subscribe(
+      data => {
+        console.log(data)
+        this.user = data
+      },
+      err => {
+        localStorage.clear()
+        this.router.navigateByUrl('/')
+      }
+    );;
+  }
+  update(data : any ){
+    this.auth.updateUser(data , this.user._id).subscribe((data)=>{
+       this.router.navigateByUrl("/createUser/Caree")
+    })
+  }
+}
