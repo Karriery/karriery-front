1a1091a3632aebbaa84e41102142255842107ac9
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-05 19:49:35 +0100

    up

--- src/app/templates/create-user-payment-versement/create-user-payment-versement.component.ts
+++ src/app/templates/create-user-payment-versement/create-user-payment-versement.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-create-user-payment-versement',
-  templateUrl: './create-user-payment-versement.component.html',
-  styleUrls: ['./create-user-payment-versement.component.css']
+  selector: "app-create-user-payment-versement",
+  templateUrl: "./create-user-payment-versement.component.html",
+  styleUrls: ["./create-user-payment-versement.component.scss"],
 })
-export class CreateUserPaymentVersementComponent {
-
-}
+export class CreateUserPaymentVersementComponent {}
