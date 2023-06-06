0dd865943c2360edb71648515192f0793265c264
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-06 14:51:40 +0100

    up

--- src/app/templates/coach-profil-dashbord/coach-profil-dashbord.component.ts
+++ src/app/templates/coach-profil-dashbord/coach-profil-dashbord.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { AuthService } from "src/app/auth.service";
+
 @Component({
   selector: 'app-coach-profil-dashbord',
   templateUrl: './coach-profil-dashbord.component.html',
   styleUrls: ['./coach-profil-dashbord.component.scss']
 })
 export class CoachProfilDashbordComponent {
-  user: any;
-  constructor(private auth: AuthService) {}
-  ngOnInit() {
-    this.auth.verify().subscribe((data) => {
-      console.log(data);
-      this.user = data;
-    });
-  }
 
 }
