10fe09bfd7025796b6bfb02b1b1226bf64f47d2c
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-06 16:36:55 +0100

    up

--- src/app/templates/coach-profi-optimisations-cv/coach-profi-optimisations-cv.component.ts
+++ src/app/templates/coach-profi-optimisations-cv/coach-profi-optimisations-cv.component.ts
@@ -3,8 +3,75 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-coach-profi-optimisations-cv',
   templateUrl: './coach-profi-optimisations-cv.component.html',
-  styleUrls: ['./coach-profi-optimisations-cv.component.css']
+  styleUrls: ['./coach-profi-optimisations-cv.component.scss']
 })
 export class CoachProfiOptimisationsCVComponent {
+  list :any=[{
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Imen Imen",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },{
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },{
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },
+  {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  }, {
+    firstname:"Abid Saif",
+    pack:"GROWTH ",
+    job:"Graphique design",
+    date:"6/04",
+    cv:"Abid_Saif_GP.pdf",
+  },]
 
 }
